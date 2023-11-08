e83000594d53a3d54357ade5f2b5121a8689bc22
daikimatsuura <matsura@geekfeed.co.jp>
2023-11-08 17:43:18 +0900

    2.1_useState_count

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import { MyButton } from "./components/atoms/MyButton";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyButton title="My Button" />
-      </header>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>＋</button>
+      <button onClick={decrement}>ー</button>
     </div>
   );
 }
