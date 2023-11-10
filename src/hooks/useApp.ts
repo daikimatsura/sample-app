1c0cd7347b99b36f34be31453739a26774b3a96a
daikimatsuura <matsura@geekfeed.co.jp>
2023-11-10 15:24:32 +0900

    3.1_custome_hooks_useApp

--- src/hooks/useApp.ts
+++ src/hooks/useApp.ts
@@ -1,18 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 
 export const useApp = () => {
   const [text, setText] = useState<string>("");
-  const [textLength, setTextLength] = useState<number>(text.length);
-
-  useEffect(() => {
-    setTextLength(text.length);
-  }, [text]);
+  const textLength = useMemo(() => text.length, [text]);
 
   // テキストボックスの値が変更されたときに呼び出される関数
-  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // textの値を更新
-    setText(e.target.value);
-  };
+  const handleTextChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      // textの値を更新
+      setText(e.target.value);
+    },
+    []
+  );
   return {
     state: { text, textLength },
     handler: { handleTextChange },
